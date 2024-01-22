fa2c42647480830e40ab5936cf7ce2fd4b15e639
WooYoungDoo <duden5000@naver.com>
2024-01-22 16:05:15 +0900

    feat: TestController

---
+++ src/main/java/com/example/test/TestController.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "hello 8080 port";
+    }
+
+}
